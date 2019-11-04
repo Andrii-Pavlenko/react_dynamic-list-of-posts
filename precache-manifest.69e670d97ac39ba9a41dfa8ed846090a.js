d527e8478c22350fc925fc2d44969dbbb1b1885f
Andrii <farikpite@gmail.com>
2019-11-04 23:16:37 +0200

    Updates

---
+++ precache-manifest.69e670d97ac39ba9a41dfa8ed846090a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05dc0ff99782362eff2fd89681b33ee",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "b334dc2d7614c57c6f3e",
+    "url": "/react_dynamic-list-of-posts/static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "3a489db0df6f882044f2",
+    "url": "/react_dynamic-list-of-posts/static/js/2.9c7cfcc2.chunk.js"
+  },
+  {
+    "revision": "b334dc2d7614c57c6f3e",
+    "url": "/react_dynamic-list-of-posts/static/js/main.3ad82868.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
